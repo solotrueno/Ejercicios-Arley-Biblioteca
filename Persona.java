3bfeac369082e6e1a871b9ff496796c466046206
Desarrollador Java Script <jhonson123456789castro@gmail.com>
2026-03-02 20:44:48 -0500

    Create Persona class with name and nationality attributes

---
+++ Persona.java
@@ -0,0 +1,20 @@
+class Persona {
+
+    private String nombre;
+    private String nacionalidad;
+
+
+    public Persona(String nombre, String nacionalidad) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+}
